5a448c0393d396f584ea95390d94c685b01246a7
Syed Fazal Mehdi <SYEDFAZAL.MEHDI10@nmims.in>
2026-04-06 05:06:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import sklearn
 import requests
 
 st.set_page_config(layout="wide")
@@ -12,27 +11,22 @@ st.markdown("""
         background-color: #0e1117;
         color: white;
     }
-
     .movie-card {
         text-align: center;
     }
-
     .movie-title {
         font-size: 16px;
         font-weight: 600;
         margin-top: 10px;
         color: white;
     }
-
     img {
         border-radius: 12px;
         transition: transform 0.3s ease;
     }
-
     img:hover {
         transform: scale(1.05);
     }
-
     .stButton>button {
         background-color: #e50914;
         color: white;
@@ -45,20 +39,41 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+OMDB_API_KEY = "81c0a085"
 
+def fetch_poster(movie_name):
+    url = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={movie_name}"
+    try:
+        data = requests.get(url, timeout=5).json()
+        if data.get('Response') == 'True':
+            return data['Poster']
+    except:
+        pass
+    return "https://via.placeholder.com/500x750?text=No+Image"
 
+@st.cache_data
+def load_data():
+    movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+    movies = pd.DataFrame(movies_dict)
+
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.metrics.pairwise import cosine_similarity
+
+    cv = CountVectorizer(max_features=5000, stop_words='english')
+    vectors = cv.fit_transform(movies['tags']).toarray()
+    similarity = cosine_similarity(vectors)
+
+    return movies, similarity
+
+movies, similarity = load_data()
 
 def recommend(movie_name):
     movie_index = movies[movies['title'] == movie_name].index[0]
     distances = similarity[movie_index]
-
-    movies_list = sorted(list(enumerate(distances)),
-                         reverse=True,
-                         key=lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
     recommended_posters = []
-
     for i in movies_list:
         title = movies.iloc[i[0]].title
         recommended_movies.append(title)
@@ -66,60 +81,22 @@ def recommend(movie_name):
 
     return recommended_movies, recommended_posters
 
-##
-import requests
-
-OMDB_API_KEY = "81c0a085"
-
-
-def fetch_poster(movie_name):
-    url = f"http://www.omdbapi.com/?apikey={OMDB_API_KEY}&t={movie_name}"
-
-    data = requests.get(url).json()
-
-    if data['Response'] == 'True':
-        return data['Poster']
-    else:
-        return "https://via.placeholder.com/500x750?text=No+Image"
-
-
-
-
-
-movies_dict=pickle.load(open('movies_dict.pkl','rb'))
-movies=pd.DataFrame(movies_dict)
-
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-
-cv = CountVectorizer(max_features=5000, stop_words='english')
-vectors = cv.fit_transform(movies['tags']).toarray()
-
-similarity = cosine_similarity(vectors)
-
-
 # Header
 st.markdown("<h1 style='text-align:center;'>🎬 Movie Recommender</h1>", unsafe_allow_html=True)
 
 # Selectbox
-selected_movie_name = st.selectbox(
-    "SELECT A MOVIE ",
-    movies['title'].values
-)
+selected_movie_name = st.selectbox("SELECT A MOVIE", movies['title'].values)
 
 # Button
-col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     recommend_clicked = st.button("Recommend")
 
 # Output
 if recommend_clicked:
     names, posters = recommend(selected_movie_name)
-
     st.markdown("<br>", unsafe_allow_html=True)
-
     cols = st.columns(5)
-
     for i in range(5):
         with cols[i]:
             st.markdown(f"""
